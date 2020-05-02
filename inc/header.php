f7a5d1ed635a624d746a9cc17b2a53d709cda6a6
Salman89dev <hmsalmansadiq@gmail.com>
2020-05-02 08:40:03 -0700

    now ready to commit

---
+++ inc/header.php
@@ -0,0 +1,377 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<!-- <link rel="stylesheet" href="./assets/css/custom.css"> -->
+<?php
+include './assets/css/style.php';
+?>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ 
+</head>
+<body>
+<section class="sec-nav">
+ <div class="container">
+   <div class="row">
+     <div class="col-md-2">
+     <i class="fa fa-facebook" aria-hidden="true"></i>
+     <i class="fa fa-twitter" aria-hidden="true"></i>
+     <i class="fa fa-instagram" aria-hidden="true"></i>
+     <i class="fa fa-whatsapp" aria-hidden="true"></i>
+     <i class="fa fa-youtube-play" aria-hidden="true"></i>
+     </div>
+     <div class="col-md-6 para">
+       <p>Millions of people's life has changed, millions more awaiting your donation</p>
+     </div>
+     <div class="col-md-4 btn">
+       <a href="#">UK Donor</a>
+       <a href="#">Donate Now</a>
+       <a href="#">SMIT Results</a>
+     </div>
+   </div>
+
+ </div>
+</section>
+
+
+<nav class="navbar navbar-margin navbar-inverse header-outer">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand " id="logo" href="#"><img src="./assets/images/Saylani-logo.png" ></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav sf-menu">
+        <li ><a href="#">Home</a></li>
+        <li class="dropdown">
+          <a class="dropdown-toggle dropbtn" data-toggle="dropdown" href="#">About <span class="caret"></span></a>
+          <ul class="dropdown-menu dropdown-content simple">
+            <li><a href="#">Introduction</a></li>
+            <li><a href="#">Chairman Message</a></li>
+            <li><a href="#">Saylani Annual Report</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Saylani Services <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Education</a><ul>
+            <li><a href="#">Salayni Mass Traning  </a></li>
+            <li><a href="#">Salayai Incubator</a></li>
+            <li><a href="#">Green HOuse</a></li>
+            </ul></li>
+            <li><a href="#">Medical</a></li>
+            <li><a href="#">Welfare</a></li>
+            <li><a href="#">Hajj Application</a></li>
+            <li><a href="#">Saylani Dastarkhwan</a></li>
+            <li><a href="#">Saylani RO Plant</a></li>
+            <li><a href="#">Online Sadqah</a></li>
+            <li><a href="#">Online Taweezat/Istekhara</a></li>
+            <li><a href="#">Saylani Job Bank</a></li>
+            <li><a href="#">Online Quran Academy</a></li>
+            <li><a href="#">Books</a></li>
+            <li><a href="#"> Saylani Online Masjid</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Future Project
+           <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Saylani University</a></li>
+            <li><a href="#">Saylani City</a></li>
+          
+          </ul>
+        </li>
+        <li><a href="#">Media</a></li>
+        <li><a href="#">News</a></li>
+        <li><a href="#">Donation</a></li>
+       
+      </ul>
+     
+    </div>
+  </div>
+</nav>
+
+<!-- SLIDER -->
+
+<div class="container-fluid slider">
+  <!-- <br> -->
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <!-- <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="3"></li>
+    </ol> -->
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+
+      <div class="item active">
+        <img class="img-responsive" src="assets/images/slider.jpg" alt="Chania" width="460" height="345">
+        <div class="carousel-caption">
+        
+        </div>
+      </div>
+
+      <div class="item">
+        <img class="img-responsive" src="assets/images/slider2.jpg" alt="Chania" width="460" height="345">
+        <!-- <div class="carousel-caption">
+          <h3>Chania</h3>
+          <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+        </div> -->
+      </div>
+    
+      <!-- <div class="item">
+        <img class="img-responsive" src="assets/images/3.jpeg" alt="Flower" width="460" height="345">
+        <div class="carousel-caption">
+          <h3>Flowers</h3>
+          <p>Beatiful flowers in Kolymbari, Crete.</p>
+        </div>
+      </div> -->
+
+      <div class="item">
+        <img class="img-responsive" src="assets/images/slider4.jpg" alt="Flower" width="460" height="345">
+        <!-- <div class="carousel-caption">
+          <h3>Flowers</h3>
+          <p>Beatiful flowers in Kolymbari, Crete.</p>
+        </div> -->
+      </div>
+  
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control " href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left background" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+<!-- SLIDER -->
+
+<!-- playing text -->
+<div class="container moving-text">
+  <p><span class="color"> news-1 Dated:15-06-2019) </span> PIAICNext Phase Admission in Islamabad And Faisalabad" .<span class="blue"> More Details </span>  
+      (News-2 Dated:15-05-2019)  Saylani Welfare Announced "Donation Application"</p>
+</div>
+<!-- PLAYING TEXT -->
+<section class="home-sec2">
+
+<div class="container intro">
+  <h1>
+  INTRO<span class="chnage">DUCTION<span>
+  </h1>
+  <img src="assets/images/icon.png" alt="">
+  <p>Established in May 1999 by famous spiritual and religious scholar Maulana 
+    Bashir Ahmed Farooqui, Saylani Welfare International Trust was built on the 
+    fundamentals of breaking the cycle of poverty, alleviating the financial troubles of the poor,
+     giving people a chance to live a dignified life and spreading happiness.</p>
+     <br>
+     <p>We are an organization that believes in lighting up the lives of underprivileged people across the world. We endeavor to provide the best quality services in areas including food, education, medical and social welfare free of cost to people living in the dark. With over 60 different sectors, we feed thousands of hungry people each day, hundreds more are given the hope of life through medical health care, many are educated to become 
+       leaders of tomorrow and several are given the opportunity to stand on their feet financially.</p>
+     <br>
+
+     <p>Today, we are proud to say that our physical presence extends throughout Pakistan
+        with a vast network of 125 branches operating in major cities including Karachi, Lahore,
+         Islamabad, Rawalpindi, Hyderabad and Faisalabad. Apart from Pakistan, we have
+          overseas offices in the UK, USA and UAE as well. Our worldwide branches are 
+          operating under the guidance of a team of over 2,00
+       0 working professionals who help almost 125,000 people on a daily basis.</p>
+       <br>
+
+       <p>We strongly believe that a little help goes a long way and our work 
+         would not be possible without the generous support of our valuable donors
+         . Our local and international donors have graciously lent us a hand by supporting
+          our causes and enabling us to serve the needs of people in need.</p>
+</div>
+</section>
+
+<section class="home-sec3"> 
+
+<div class="container">
+  <div class="row">
+  <div class="col-md-3 hello ">
+    <img  class="img-responsive" src="assets/images/spoon.png" alt="">
+    <h1>125,000</h1>
+    <p>DAILY DASTRAKHAN<span class="second">(MONTHLY)</span></p>
+    
+  </div>
+  <div class="col-md-3 hello">
+    <img  class="img-responsive" src="assets/images/family.png" alt="">
+    <h1>7,500</h1>
+    <p>FAMILY ADOPTION<span class="second">(MONTHLY)</span></p>
+    
+  </div>
+  <div class="col-md-3 hello">
+    <img  class="img-responsive" src="assets/images/doctor.png" alt="">
+    <h1>43,749</h1>
+    <p>MEDICAL<span class="second">(MONTHLY)</span></p>
+    
+  </div>
+  <div class="col-md-3 hello">
+    <img  class="img-responsive" src="assets/images/icon_education_png.png" alt="">
+    <h1>180,000</h1>
+    <p>EDUCATION<span class="second">(MONTHLY)</span></p>
+    
+  </div>
+  </div>
+</div>
+</section>
+
+<section class="home-sec4">
+ <div class="container">
+   <div class="row">
+     <div class="col-md-4 ">
+         <div class="box1">
+          <img src="assets/images/sadqah.png" alt="">
+          <h2>ONLINE SADQAH</h2>
+         </div>
+     </div> 
+     <div class="col-md-4 ">
+         <div class="box1">
+          <img src="assets/images/sadqah.png" alt="">
+          <h2>ONLINE SADQAH</h2>
+         </div>
+     </div>
+     <div class="col-md-4 ">
+         <div class="box1">
+          <img src="assets/images/sadqah.png" alt="">
+          <h2>ONLINE SADQAH</h2>
+         </div>
+     </div> 
+   </div>
+   <div class="row">
+     <div class="col-md-4 ">
+         <div class="box1">
+          <img src="assets/images/sadqah.png" alt="">
+          <h2>ONLINE SADQAH</h2>
+         </div>
+     </div> 
+     <div class="col-md-4 ">
+         <div class="box1">
+          <img src="assets/images/sadqah.png" alt="">
+          <h2>ONLINE SADQAH</h2>
+         </div>
+     </div>
+     <div class="col-md-4 ">
+         <div class="box1">
+          <img src="assets/images/sadqah.png" alt="">
+          <h2>ONLINE SADQAH</h2>
+         </div>
+     </div> 
+   </div>
+</section>
+
+<section class="home-sec-mis"> 
+ <div class="container">
+   <div class="row">
+      <div class="col-md-8">
+         <h2>MISSION AND UPCOMING PROJECTS</h2>
+          <div class="row">
+            <div class="col-md-6  ">
+                 <div class="hepa flex">
+                    <img src="assets/images/hepatitis.png" alt="">
+                    <h4>HEPATITIS</h4>
+                 </div>
+            </div>
+            <div class="col-md-6">
+               <div class="hepa flex">
+                 <img src="assets/images/house socirty.png" alt="">
+                 <h4>HOUSING SOCIETY</h4>
+               </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 ">
+                <div class="hepa flex">
+                   <img src="assets/images/IT.png" alt="">
+                   <h4>IT LITERACY</h4>
+                </div>
+            </div>
+            <div class="col-md-6">
+                  <div class="hepa flex">
+                    <img src="assets/images/tharpakaar.png" alt="">
+                    <h4>THARPARKAR</h4>
+                  </div>
+            </div>
+          </div>
+      </div>
+
+      <div class="col-md-4">
+        <h2>Upcoming Events</h2>
+        <p>Saylani Welfare Distributing Ration & Clothes to affected people of Balochistan .
+           More Details Entry Test Accouncement (Graphic Designing & Video Editing/Animation Course)
+           On Sunday 3rd March 2019 @ 11:00Am Vanue: 4rh Floor, Saylani Welfare International Trust
+           Saylani Welfare Distributing Ration & Clothes to affected people of Balochistan .More Details
+          Entry Test Accouncement (Graphic Designing & Video Editing/Animation Course) On Sunday 3rd March 2019 @ 
+          11:00Am Vanue: 4rh Floor, Saylani Welfare International Trust HeadOffice Bahadurabad .More Details
+        </p>
+      </div>
+   </div>
+ </div>
+</section>
+
+<section class="home-sec5">
+
+<div class="container intro">
+  <h1>
+  IMAGE<span class="chnage">GALLERY<span>
+  </h1>
+  <img src="assets/images/icon.png" alt="">
+  </div>
+</section>
+<div class="container home-sec6">
+  <div class="images">
+    <img  src="assets/images/1.jpg" alt="">
+    <img src="assets/images/2.jpg" alt="">
+    <img src="assets/images/3.jpg" alt="">
+    <img src="assets/images/4.jpg" alt="">
+    <img src="assets/images/5.jpg" alt="">
+  </div>
+</div>
+<section class="footer">
+  <div class="container">
+    <div class="col-md-4 first-col">
+      <ul>
+       <div class="seperate">
+       <li>Head Office,</li>
+        <li> A-25, Bahadurabad Chowrangi Karachi, Pakistan</li>
+       </div>
+        <li><i class="fa fa-phone" aria-hidden="true"></i>  UAN: 111-729-526           <span class="world"><i class="fa fa-globe" aria-hidden="true"></i>(+0092-213) 4130786-90 </span></li>
+        <li><i class="fa fa-phone" aria-hidden="true"></i>CELL: 92-311-1729526</li>
+        <li><i class="fa fa-phone" aria-hidden="true"></i>USA NO +1(716)941 7792</li>
+        <li><i class="fa fa-phone" aria-hidden="true"></i>UK NO (+44)115 970 6256</li>
+        <li><i class="fa fa-envelope-o" aria-hidden="true"></i> info@saylaniwelfare.com</li>
+      </ul>
+    </div>
+    <div class="col-md-4 mid">
+      <h5>Useful Links</h5>
+    </div>
+    <div class="col-md-4 left">
+      <h5>Connect With Us</h5>
+      <i class="fa fa-facebook" aria-hidden="true"></i>
+     <i class="fa fa-twitter" aria-hidden="true"></i>
+     <i class="fa fa-instagram" aria-hidden="true"></i>
+     <i class="fa fa-whatsapp" aria-hidden="true"></i>
+     <i class="fa fa-youtube-play" aria-hidden="true"></i>
+    </div>
+  </div>
+
+</section>
+
+</body>
+</html>
+
+    
+</body>
+</html>
